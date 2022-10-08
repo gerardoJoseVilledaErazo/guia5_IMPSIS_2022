b10d7c63670261bbbaa6e4f5ea013419eec10033
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-10-07 23:54:43 -0600

    se modificaron los archivos config y database de la carpeta config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/guia5_2022/';
-//$config['base_url'] = 'http://localhost/guia5_2022/';
+//$config['base_url'] = 'http://localhost:8080/guia5_2022/';
+$config['base_url'] = 'http://localhost/guia5_2022/';
 
 /*
 |--------------------------------------------------------------------------
